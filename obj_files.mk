8d0bf9e43693d30355d9e0013e2e43d53889277c
Nick <NicholasMoser56@gmail.com>
2020-12-30 14:28:27 -0600

    Rename hsdbase_2 to bytecode

--- obj_files.mk
+++ obj_files.mk
@@ -37,7 +37,7 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/sysdolphin/video.o             \
 	$(BUILD_DIR)/asm/sysdolphin/hsdbase_1.o         \
 	$(BUILD_DIR)/src/sysdolphin/random.o            \
-	$(BUILD_DIR)/asm/sysdolphin/hsdbase_2.o         \
+	$(BUILD_DIR)/asm/sysdolphin/bytecode.o         \
 	$(BUILD_DIR)/asm/sysdolphin/class.o             \
 	$(BUILD_DIR)/asm/sysdolphin/hash.o              \
 	$(BUILD_DIR)/asm/sysdolphin/texp.o              \
