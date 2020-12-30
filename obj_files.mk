91211644aecec572b559e51073a44f2af2d69f22
Nick <NicholasMoser56@gmail.com>
2020-12-30 13:29:32 -0600

    Rename text_3 to hsdbase_2

--- obj_files.mk
+++ obj_files.mk
@@ -14,7 +14,7 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/src/unlock.o                       \
 	$(BUILD_DIR)/asm/text_2.o                       \
 	$(BUILD_DIR)/src/sysdolphin/random.o            \
-	$(BUILD_DIR)/asm/text_3.o                       \
+	$(BUILD_DIR)/asm/hsdbase_2.o                    \
 	$(BUILD_DIR)/asm/code_801d5ed8.o                \
 	$(BUILD_DIR)/asm/gobjproc.o                     \
 	$(BUILD_DIR)/asm/gobjplink.o                    \
