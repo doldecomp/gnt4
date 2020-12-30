1c74b6323201627934aaa93ba4917e93495e0f28
Nick <NicholasMoser56@gmail.com>
2020-12-30 13:15:12 -0600

    Move gobj and gobjuserdata out of text_3

--- obj_files.mk
+++ obj_files.mk
@@ -15,6 +15,8 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/text_2.o                       \
 	$(BUILD_DIR)/src/sysdolphin/random.o            \
 	$(BUILD_DIR)/asm/text_3.o                       \
+	$(BUILD_DIR)/asm/gobjuserdata.o                 \
+	$(BUILD_DIR)/asm/gobj.o                         \
 	$(BUILD_DIR)/asm/hvqm4dec.o                     \
 	$(BUILD_DIR)/asm/hvqm4decsnd.o                  \
 	$(BUILD_DIR)/asm/musyx.o                        \
