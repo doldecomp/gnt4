d7f2817bde6161fc3082dd1ec2ba7e66103e907f
Nick <NicholasMoser56@gmail.com>
2021-01-03 15:14:02 -0600

    Add function names for db asm

--- asm/db/db.s
+++ asm/db/db.s
@@ -2,19 +2,21 @@
 
 .section .text  # 0x8016D51C - 0x8016D608
 
-.global func_8016D51C
-func_8016D51C:
+.global DBInit
+DBInit:
 /* 8016D51C 0016A51C  3C 80 80 00 */	lis r4, 0x80000040@ha
 /* 8016D520 0016A520  38 04 00 40 */	addi r0, r4, 0x80000040@l
-/* 8016D524 0016A524  3C 60 80 17 */	lis r3, lbl_8016D58C@ha
+/* 8016D524 0016A524  3C 60 80 17 */	lis r3, __DBExceptionDestination@ha
 /* 8016D528 0016A528  90 0D 8B F0 */	stw r0, lbl_80277510-_SDA_BASE_(r13)
-/* 8016D52C 0016A52C  38 63 D5 8C */	addi r3, r3, lbl_8016D58C@l
+/* 8016D52C 0016A52C  38 63 D5 8C */	addi r3, r3, __DBExceptionDestination@l
 /* 8016D530 0016A530  3C 03 80 00 */	addis r0, r3, 0x8000
 /* 8016D534 0016A534  90 04 00 48 */	stw r0, 0x48(r4)
 /* 8016D538 0016A538  38 00 00 01 */	li r0, 1
 /* 8016D53C 0016A53C  90 0D 8B F4 */	stw r0, lbl_80277514-_SDA_BASE_(r13)
 /* 8016D540 0016A540  4E 80 00 20 */	blr 
-lbl_8016D544:
+
+.global __DBExceptionDestinationAux
+__DBExceptionDestinationAux:
 /* 8016D544 0016A544  7C 08 02 A6 */	mflr r0
 /* 8016D548 0016A548  3C 60 80 21 */	lis r3, lbl_80216160@ha
 /* 8016D54C 0016A54C  90 01 00 04 */	stw r0, 4(r1)
@@ -33,14 +35,16 @@ lbl_8016D544:
 /* 8016D580 0016A580  38 21 00 18 */	addi r1, r1, 0x18
 /* 8016D584 0016A584  7C 08 03 A6 */	mtlr r0
 /* 8016D588 0016A588  4E 80 00 20 */	blr 
-lbl_8016D58C:
+
+.global __DBExceptionDestination
+__DBExceptionDestination:
 /* 8016D58C 0016A58C  7C 60 00 A6 */	mfmsr r3
 /* 8016D590 0016A590  60 63 00 30 */	ori r3, r3, 0x30
 /* 8016D594 0016A594  7C 60 01 24 */	mtmsr r3
-/* 8016D598 0016A598  4B FF FF AC */	b lbl_8016D544
+/* 8016D598 0016A598  4B FF FF AC */	b __DBExceptionDestinationAux
 
-.global func_8016D59C
-func_8016D59C:
+.global __DBIsExceptionMarked
+__DBIsExceptionMarked:
 /* 8016D59C 0016A59C  80 8D 8B F0 */	lwz r4, lbl_80277510-_SDA_BASE_(r13)
 /* 8016D5A0 0016A5A0  54 60 06 3E */	clrlwi r0, r3, 0x18
 /* 8016D5A4 0016A5A4  38 60 00 01 */	li r3, 1
@@ -49,8 +53,8 @@ func_8016D59C:
 /* 8016D5B0 0016A5B0  7C 83 00 38 */	and r3, r4, r0
 /* 8016D5B4 0016A5B4  4E 80 00 20 */	blr 
 
-.global func_8016D5B8
-func_8016D5B8:
+.global DBPrintf
+DBPrintf:
 /* 8016D5B8 0016A5B8  94 21 FF 90 */	stwu r1, -0x70(r1)
 /* 8016D5BC 0016A5BC  40 86 00 24 */	bne cr1, lbl_8016D5E0
 /* 8016D5C0 0016A5C0  D8 21 00 28 */	stfd f1, 0x28(r1)
